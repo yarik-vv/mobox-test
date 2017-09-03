4d8bce469f4fef99366ed2c2837e17983dee66f3
yarik-vv <yarik.vashchuk@gmail.com>
2017-09-03 07:57:41 +0300

    gh-pages build

--- source/build.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import scss from './scss/index.scss';
-import data from './posts.json';
-import mainJS from 'file-loader?name=[path][name].[ext]!./js/index.js';
-import dropdownJS from 'file-loader?name=[path][name].[ext]!./js/dropdown.js';
-import paginationJS from 'file-loader?name=[path][name].[ext]!./js/pagination.js';
-import searchJS from 'file-loader?name=[path][name].[ext]!./js/search.js';
